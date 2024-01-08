5049f1eed03b631f3e8fea885bdc9848df851d63
hassanadawieh <adawiehhassan@gmail.com>
2024-01-08 13:24:10 +0200

    testing24

--- src/app/api/message/send/route.ts
+++ src/app/api/message/send/route.ts
@@ -52,17 +52,21 @@ export async function POST(req: Request) {
     const message = messageValidator.parse(messageData);
 
     // notify all connected chat room clients
-     pusherServer.trigger(
+    await pusherServer.trigger(
       toPusherKey(`chat:${chatId}`),
       "incoming_message",
       message
     );
     console.log("message , from the server");
-      pusherServer.trigger(toPusherKey(`user:${friendId}:chats`), "new_message", {
-      ...message,
-      senderImage: sender.image,
-      senderName: sender.name,
-    });
+    await pusherServer.trigger(
+      toPusherKey(`user:${friendId}:chats`),
+      "new_message",
+      {
+        ...message,
+        senderImage: sender.image,
+        senderName: sender.name,
+      }
+    );
 
     // all valid, send the message
     await db.zadd(`chat:${chatId}:messages`, {
